36cba26222dd3682ff3ea49499316fa0ab5bdc2f
Lpz098 <feligamer560@gmail.com>
2026-07-11 14:05:01 -0300

    tabuada

---
+++ leetcode sla oq.py
@@ -0,0 +1,18 @@
+from ast import List
+
+
+def maximumWealth( accounts: List[List[int]]) -> int:
+        maior = 0
+
+        for cliente in accounts:
+            total = 0
+
+            for conta in cliente:
+                total += conta
+
+            if total > maior:
+                maior = total
+
+        return maior
+    
+print(maximumWealth([[1,2,3],[3,2,1]]))  # Output: 6
