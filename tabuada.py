864448e752a8d6baf4a71aded9217134086a5e5a
Lpz098 <feligamer560@gmail.com>
2026-07-13 10:19:43 -0300

    tabuada

---
+++ tabuada.py
@@ -0,0 +1,12 @@
+
+num = int(input("Digite um número para ver a tabuada: "))
+def tabuada(num):
+    tabuada = []
+    print(f"Tabuada do {num}:")
+    for i in range(1, 11):
+        resultado = num * i
+        tabuada.append(resultado)
+    return tabuada
+
+
+print(tabuada(num))
