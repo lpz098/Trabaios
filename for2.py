dbb33f5f3a1e2dd2927f563539d63b6fd963c38b
Lpz098 <feligamer560@gmail.com>
2026-07-13 10:49:38 -0300

    while/for2

---
+++ for2.py
@@ -0,0 +1,6 @@
+nums = [1, 2, 3, 4, 5]
+total = 0
+for num in nums:
+    total += num
+
+print(total)
---
+++ while.py
@@ -0,0 +1,9 @@
+numero = int(input("Digite um número (0 para sair): "))
+quantidade = 0
+soma = 0
+while numero != 0:
+    soma += numero
+    quantidade += 1
+    numero = int(input("Digite um número (0 para sair): "))
+media = soma / quantidade if quantidade > 0 else 0
+print(f"A soma é {soma} e a média é {media}")
